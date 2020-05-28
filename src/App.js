e1b868557a4f165708d26d984ff81f8fa48a07cc
Jacob Stanger <jstangwork@gmail.com>
2020-05-27 20:10:13 -0600

    hooked up bootstrap

--- src/App.js
+++ src/App.js
@@ -3,38 +3,45 @@ import logo from './logo.svg';
 import Container from 'react-bootstrap/Container';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        <Jumbotron>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand>Brand text</Navbar.Brand>
-      </Navbar>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Hi</h1>
-        </Jumbotron>
-      </Container>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello this is my portfolio in  <code>React</code>.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <article>
-        <p>stuff here</p>
-      </article>
-    </div>
+    <>
+  <Navbar bg="light">
+    <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+  </Navbar>
+  <br />
+  <Navbar bg="light">
+    <Navbar.Brand>Brand text</Navbar.Brand>
+  </Navbar>
+  <br />
+  <Navbar bg="dark">
+    <Navbar.Brand href="#home">
+      <img
+        src="/logo.svg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+    </Navbar.Brand>
+  </Navbar>
+  <br />
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">
+      <img
+        alt=""
+        src="/logo.svg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />{' '}
+      React Bootstrap
+    </Navbar.Brand>
+  </Navbar>
+</>
   );
 }
 
