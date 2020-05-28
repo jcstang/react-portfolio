04fc6d9f8951d918d8f8c09dc06da64efc3abaf8
Jacob Stanger <jstangwork@gmail.com>
2020-05-28 12:30:29 -0600

    col inside media boxes

--- src/App.js
+++ src/App.js
@@ -4,28 +4,35 @@ import NavbarComp from './components/NavbarComp';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import headshot from './headshot-trees.png';
-import headshot2 from './headshot-2.png';
+import headshot from './headshot-2.png';
+import Media from 'react-bootstrap/Media';
 
 function App() {
   return (
     <div className="home-me">
       <NavbarComp />
-      <Container fluid>
-        <Row>
-          <Col md={4}>
-            <img 
-              src={headshot2} 
-              className="App-headshot" 
-              alt="headshot"
+      <Container fluid className="main-content">
+      <Row>
+        <Media>
+          <Col md={3}>
+            <img
+              width={180}
+              height={180}
+              className="align-self-start mr-3"
+              src={headshot}
+              alt="Generic placeholder"
             />
           </Col>
-          <Col md={8}>
-            <p>
-            Full stack web developer with a passion for iOS development. Educated in computer science at Weber State University and recently earned a coding certificate from the University of Utah. Life-long learner that thrives in team environments and is driven by complex problem solving with more than 100 hours on Udemy and Pluralsight. Expertise interfacing and scoping solutions with clients and clearly explaining technical topics to stakeholders.
-            </p>
+          <Col md={9}>
+            <Media.Body>
+              <h5>About me</h5>
+              <p>
+                Full stack web developer with a passion for iOS development. Educated in computer science at Weber State University and recently earned a coding certificate from the University of Utah. Life-long learner that thrives in team environments and is driven by complex problem solving with more than 100 hours on Udemy and Pluralsight. Expertise interfacing and scoping solutions with clients and clearly explaining technical topics to stakeholders.
+              </p>
+            </Media.Body>
           </Col>
-        </Row>
+        </Media>
+      </Row>
       </Container>
     </div>
   );
