0254478a9724bd4b736d5a60a3b78332f4b6c9e6
Jacob Stanger <jstangwork@gmail.com>
2020-05-27 20:04:04 -0600

    setup navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Container>
+        <Jumbotron>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand>Brand text</Navbar.Brand>
+      </Navbar>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hi</h1>
+        </Jumbotron>
+      </Container>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello this is my portfolio in  <code>React</code>.
         </p>
         <a
           className="App-link"
@@ -19,6 +31,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <article>
+        <p>stuff here</p>
+      </article>
     </div>
   );
 }
