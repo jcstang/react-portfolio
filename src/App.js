48d0e1db2982a1f06f671e72259e6aad2d53baf1
Jacob Stanger <jstangwork@gmail.com>
2020-05-27 20:16:08 -0600

    great first setup of my portfolio site

--- src/App.js
+++ src/App.js
@@ -1,47 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
-import Container from 'react-bootstrap/Container';
-import Jumbotron from 'react-bootstrap/Jumbotron';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import './App.css';
 
 function App() {
   return (
     <>
-  <Navbar bg="light">
-    <Navbar.Brand href="#home">Brand link</Navbar.Brand>
-  </Navbar>
-  <br />
-  <Navbar bg="light">
-    <Navbar.Brand>Brand text</Navbar.Brand>
-  </Navbar>
-  <br />
-  <Navbar bg="dark">
-    <Navbar.Brand href="#home">
-      <img
-        src="/logo.svg"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-        alt="React Bootstrap logo"
-      />
-    </Navbar.Brand>
-  </Navbar>
-  <br />
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">
-      <img
-        alt=""
-        src="/logo.svg"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-      />{' '}
-      React Bootstrap
-    </Navbar.Brand>
-  </Navbar>
-</>
+      <Navbar bg="dark" variant="dark">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Navbar.Brand href="#home">Jacob Stanger</Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   );
 }
 
