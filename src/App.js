43ed86ac63c976afbca477b46b28c3a83beba5c6
Jacob Stanger <jstangwork@gmail.com>
2020-05-28 11:31:34 -0600

    adding headshot and bootstrap containers

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import './App.css';
+import NavbarComp from './components/NavbarComp';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import headshot from './headshot-trees.png';
+import headshot2 from './headshot-2.png';
 
 function App() {
   return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Navbar.Brand href="#home">Jacob Stanger</Navbar.Brand>
-
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </>
+    <div className="home-me">
+      <NavbarComp />
+      <Container fluid>
+        <Row>
+          <Col md={4}>
+            <img 
+              src={headshot2} 
+              className="App-headshot" 
+              alt="headshot"
+            />
+          </Col>
+          <Col md={8}>
+            <p>
+            Full stack web developer with a passion for iOS development. Educated in computer science at Weber State University and recently earned a coding certificate from the University of Utah. Life-long learner that thrives in team environments and is driven by complex problem solving with more than 100 hours on Udemy and Pluralsight. Expertise interfacing and scoping solutions with clients and clearly explaining technical topics to stakeholders.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
