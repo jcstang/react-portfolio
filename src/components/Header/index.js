30517cf9bd84b476edcab5d47b32ea23dad1319a
Jacob Stanger <jstangwork@gmail.com>
2020-06-02 17:24:26 -0600

    add email to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -16,10 +16,16 @@ export default function Header() {
             />
           </div>
           <div className="col-md-9">
+            <h5 className="title is-2">Jacob Stanger</h5>
             <h5 className="title is-3">About me</h5>
             <p>
               Full stack web developer with a passion for iOS development. Educated in computer science at Weber State University and recently earned a coding certificate from the University of Utah. Life-long learner that thrives in team environments and is driven by complex problem solving with more than 100 hours on Udemy and Pluralsight. Expertise interfacing and scoping solutions with clients and clearly explaining technical topics to stakeholders.
             </p>
+            <hr/>
+            <p><strong>Email</strong> JacobcStanger@gmail.com</p>
+            <div className="row">
+              {/* Add social links here too? or written out? */}
+            </div>
           </div>
         </div>
       </div>
