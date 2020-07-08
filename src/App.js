2f31ecb811cc7cc99c500cfc2dea87e657954f52
Jacob Stanger <jacobcstanger@gmail.com>
2020-07-08 11:59:42 -0600

    fixed 2 warnings. unused.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import React, { useState } from 'react';
-import NavbarComp from './components/NavbarComp';
-import CardContainer from './containers/CardContainer';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import initData from './data/init-data';
-import TechSection from './components/TechSection';
-import 'bulma/css/bulma.css';
-import './App.css';
-import TileContainer from './containers/TileContainer';
+import React, { useState } from "react";
+import NavbarComp from "./components/NavbarComp";
+import CardContainer from "./containers/CardContainer";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import initData from "./data/init-data";
+import TechSection from "./components/TechSection";
+import "bulma/css/bulma.css";
+import "./App.css";
 
 function App() {
-
   // NEED to change titles or screenshots? go edit ./data/init-data
   // using State
-  const [ firstRowState ] = useState(initData.row1);
-  const [ secondRowState ] = useState(initData.row2);
-  const [ thirdRowState ] = useState(initData.row3);
+  const [firstRowState] = useState(initData.row1);
+  const [secondRowState] = useState(initData.row2);
+  const [thirdRowState] = useState(initData.row3);
 
-  const [ firstTechRowState ] = useState(initData.techRow1); 
-  const [ secondTechRowState ] = useState(initData.techRow2); 
+  const [firstTechRowState] = useState(initData.techRow1);
+  const [secondTechRowState] = useState(initData.techRow2);
 
   return (
     <>
       <NavbarComp />
       <Header />
 
-      <TechSection techList={firstTechRowState}/>
+      <TechSection techList={firstTechRowState} />
 
       <CardContainer cardList={firstRowState} />
       <CardContainer cardList={secondRowState} />
       <CardContainer cardList={thirdRowState} />
 
-      <TechSection techList={secondTechRowState}/>
+      <TechSection techList={secondTechRowState} />
       <Footer />
     </>
   );
--- src/components/TechSection/index.js
+++ src/components/TechSection/index.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import TechCard from '../TechCard';
-import TileContainer from '../../containers/TileContainer';
+import React from "react";
+import TechCard from "../TechCard";
 
 export default function TechSection(props) {
-
   return (
     <div className="container is-fluid">
       <div className="columns">
-        {
-          props.techList.map((tech, index) => (
-            <TechCard
-              title={tech.title}
-              key={index}
-              iconCode={tech.iconCode}
-              sizeCode={tech.sizeCode}
-            />
-          ))
-        }
+        {props.techList.map((tech, index) => (
+          <TechCard
+            title={tech.title}
+            key={index}
+            iconCode={tech.iconCode}
+            sizeCode={tech.sizeCode}
+          />
+        ))}
       </div>
     </div>
-
   );
 }
