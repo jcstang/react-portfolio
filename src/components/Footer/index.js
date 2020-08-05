b2b10b1492ba9e86f23d26e3052c13b75a572d21
Jacob Stanger <jacobcstanger@gmail.com>
2020-08-05 12:03:18 -0600

    .

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -25,6 +25,15 @@ export default function Footer() {
     }
   }
 
+  // $.getJSON('http://api.open-notify.org/astros.json?callback=?', function(data) {
+//     var number = data['number'];
+//     $('#spacepeeps').html(number);
+
+//     data['people'].forEach(function (d) {
+//          $('#astronames').append('<li>' + d['name'] + '</li>');
+//     });
+// });
+
 
   return (
     <footer className="container is-fluid footer btm-footer">
